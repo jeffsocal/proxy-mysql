bc32983ccc7a22ce5637b9b66baefdf799d2d213
Jeff <jeff@socalbioinformatics.com>
2021-03-10 10:14:53 -0800

    update to infer port from ip string

--- src/Base/Credentials.php
+++ src/Base/Credentials.php
@@ -22,21 +22,35 @@ class Credentials extends Log
     protected $password;
 
     //
-    public function __construct($server, $port = 3306)
+    public function __construct(string $server, int $port = 3306)
     {
         parent::__construct('sql');
+
+        if (! preg_match("/\d+\.\d+\.\d+\.\d+/", $server))
+            systemError("bad ip address: " . $server);
+
+        # sometimes the port gets integrated with the server ip address
+        # such as 123.45.67.89:3396
+        # we need then to over-ride the explicit port and seperate the
+        # ip:port strings
+        
+        if (preg_match("/\d+\.\d+\.\d+\.\d+\:\d+/", $server)) {
+            $port = preg_replace("/.+\:/", "", $server);
+            $server = preg_replace("/\:.+/", "", $server);
+        }
+
         $this->setSqlCredentials($server, $port);
     }
 
     //
-    private function setSqlCredentials($server, $port = 3306)
+    private function setSqlCredentials(string $server, int $port = 3306)
     {
         $this->server = $server;
         $this->port = $port;
 
         $this->login = '';
         $this->password = '';
-        
+
         if (file_exists(get_include_path() . 'ini/' . $server . "." . $port . ".ini")) {
             $sql_ini = parse_ini_file(get_include_path() . 'ini/' . $server . "." . $port . ".ini");
             $this->login = $sql_ini['login'];
